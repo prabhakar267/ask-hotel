8c37739c09eacfe7cb4e0cff3193d08cbb95357e
Mayank Badola <badola21295@gmail.com>
2017-03-26 02:26:28 +0530

    JSON data file CRU

--- server/app.py
+++ server/app.py
@@ -1,38 +1,42 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 from flask_cors import CORS
+import json
 
-from utils import add_request_to_panel
+from utils import add_request_to_panel, add_to_conventional_data_store as add, update_on_conventional_data_store as update
 from config import HOSTNAME, USERNAME, PASSWORD, DATABASE
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 CORS(app)
-
-db = MySQLdb.connect(
-	host=HOSTNAME,
-	user=USERNAME,
-	passwd=PASSWORD,
-	db=DATABASE
-)
-# cursor = db.cursor()
-
-# @app.route('/food', methods=['GET'])
-# def food():
-
-
-@app.route('/service', methods=['GET'])
-def service():
-	device_id = request.args.get("device")
-	query = request.args.get("query")
-
-	add_request_to_panel(db, device_id, query, 2)
-
 
 @app.route('/', methods=['GET'])
 def main():
 	return "Hello world", 200
 
 
+@app.route('/services', methods=['GET', 'POST'])
+def service():
+	if request.method == 'POST':
+		device_id = request.args.get("device")
+		query = request.args.get("query")
+
+		if add_request_to_panel(device_id, query, 2):
+			return "ok"
+
+		return "not ok"
+	else if request.method == 'GET':
+		return send_from_directory('static', 'servicesTodo.json')
+
+@app.route('/update/services', methods=['GET'])
+def update_services():
+	req_id = int(request.args.get("id"))
+	mode = int(request.args.get("mode"))
+
+	if update(req_id, mode):
+		return "ok"
+
+	return "not ok"
+
+
 if __name__ == "__main__":
 	app.run(debug=True, host="0.0.0.0", threaded=True)
-
--- server/utils.py
+++ server/utils.py
@@ -1,9 +1,50 @@
-def get_room_from_device(db, device_id):
+import json
+import time
+
+def read_from_conventional_data_store():
+	with open('static/servicesTodo.json') as json_data:
+		d = json.loads(json_data.read())
+		return d
+
+def add_to_conventional_data_store(room_id, query, mode):
+	store = read_from_conventional_data_store()
+	item = {}
+	item['id'] = len(store)
+	item['room_id'] = room_id
+	item['query'] = query
+	item['mode'] = mode
+	item['created_tx'] = time.time()
+	store.append(item)
+	with open('static/servicesTodo.json', 'r+') as f:
+	    text = f.read()
+	    f.seek(0)
+	    f.write(json.dumps(store))
+	    f.truncate()
+
+def update_on_conventional_data_store(index, mode):
+	store = read_from_conventional_data_store()
+
+	if index >= len(store):
+		return False
+
+	store[index]['mode'] = mode
+	with open('static/servicesTodo.json', 'r+') as f:
+	    text = f.read()
+	    f.seek(0)
+	    f.write(json.dumps(store))
+	    f.truncate()
+
+	return True
+
+
+
+
+def get_room_from_device(device_id):
 	return 1
 	cursor = db.cursor()
-	
+
 	query = "SELECT id FROM `rooms` WHERE device_id = '%s' LIMIT 1" % (device_id)
-	
+
 	cursor.execute(query)
 	row = cursor.fetchone()
 
@@ -14,20 +55,21 @@ def get_room_from_device(db, device_id):
 
 
 
-def add_request_to_panel(db, device_id, user_query, mode):
-	room_id = get_room_from_device(db, device_id)
+def add_request_to_panel(device_id, user_query, mode):
+	room_id = get_room_from_device(device_id)
 	if not room_id:
 		return False
 
-	database_query = "INSERT INTO `requests` (room_id, request, created_tx, mode) VALUES ('%d', '%s', '%s', '%d')" % (room_id, user_query, time.time(), mode)
-	cursor = db.cursor()
-	try:
-		cursor.execute(database_query)
-		db.commit()
-		# user_id = cursor.lastrowid
-		success = True
-	except e:
-		db.rollback()
-		success = False
+	# database_query = "INSERT INTO `requests` (room_id, request, created_tx, mode) VALUES ('%d', '%s', '%s', '%d')" % (room_id, user_query, time.time(), mode)
+	# cursor = db.cursor()
+	# try:
+	# 	cursor.execute(database_query)
+	# 	db.commit()
+	# 	# user_id = cursor.lastrowid
+	# 	success = True
+	# except e:
+	# 	db.rollback()
+	# 	success = False
+	add_to_conventional_data_store(device_id, user_query, mode)
 
-	return success
+	return True
