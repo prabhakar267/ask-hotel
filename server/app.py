0ed3aaee2ff74099e98b8c89107fe0f346d6946b
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2017-03-26 02:56:03 +0530

    dashboard added

--- server/app.py
+++ server/app.py
@@ -3,7 +3,9 @@ from flask_cors import CORS
 import json
 
 from utils import add_request_to_panel, add_to_conventional_data_store as add, update_on_conventional_data_store as update
-from config import HOSTNAME, USERNAME, PASSWORD, DATABASE
+
+
+PRODUCT_NAME = "hotel shit"
 
 
 app = Flask(__name__, static_url_path='')
@@ -24,7 +26,7 @@ def service():
 			return "ok"
 
 		return "not ok"
-	else if request.method == 'GET':
+	elif request.method == 'GET':
 		return send_from_directory('static', 'servicesTodo.json')
 
 @app.route('/update/services', methods=['GET'])
@@ -36,6 +38,18 @@ def update_services():
 		return "ok"
 
 	return "not ok"
+
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard_index():
+	ctx = {"product_name" : PRODUCT_NAME}
+	return render_template('index.html', **ctx)
+
+
+@app.route('/dashboard/jobs', methods=['GET'])
+def dashboard_jobs():
+	ctx = {"product_name" : PRODUCT_NAME}
+	return render_template('jobs.html', **ctx)
 
 
 if __name__ == "__main__":
